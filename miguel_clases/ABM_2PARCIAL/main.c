2ebbf117077c7b3f66fad736531e914fc82bb18c
Miguel Alvarez <49586444+miguelalvarez-utn@users.noreply.github.com>
2019-05-02 22:13:26 -0300

    Add files via upload

--- miguel_clases/ABM_2PARCIAL/main.c
+++ miguel_clases/ABM_2PARCIAL/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+struct-
+asociados:
+estadoAsociado;
+idasociado:
+nombre
+apellido
+edad
+dni
 
+llama:
+estadoLlamada:
+idasociado:
+idllamada:
+motivo:
+tiempo:
+idAmbulacia:
+
+ */
 int main()
 {
     printf("Hello world!\n");
