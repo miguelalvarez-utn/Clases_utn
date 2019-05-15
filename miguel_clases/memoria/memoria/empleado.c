4c0544860b1c41f35f01241e66d0508ff533c256
Miguel Alvarez <49586444+miguelalvarez-utn@users.noreply.github.com>
2019-05-14 22:02:20 -0300

    Add files via upload

---
+++ miguel_clases/memoria/memoria/empleado.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "empleado.h"
+#define NAME_MAX_CHAR 31
+Empleado* Emp_new(void)
+{
+    return (Empleado*) malloc(sizeof(Empleado));
+}
+//-----------------------------------------
+int Emp_delete(Empleado* this)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        free(this);
+        retorno = 0;
+    }
+    return retorno;
+}
+//---------------------------------------------
+int Emp_setId(Empleado* this, int id)
+{
+    int retorno = -1;
+    if(this != NULL && id >= 0)
+    {
+        this->id = id;
+        retorno = 0;
+    }
+    return retorno;
+}
+//--------------------------------------------
+int Emp_getId(Empleado* this, int* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->id;
+        retorno = 0;
+    }
+    return retorno;
+}
+//--------------------------------------------
+int Emp_setPeso(Empleado* this, float peso)
+{
+    int retorno = -1;
+    if(this != NULL && peso >= 0)
+    {
+        this->peso = peso;
+        retorno = 0;
+    }
+    return retorno;
+}
+//-------------------------------------------------
+int Emp_getPeso(Empleado* this, float* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->peso;
+        retorno = 0;
+    }
+    return retorno;
+}
+//-------------------------------------------------
+int Emp_setNombre(Empleado* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && nombre != NULL)
+    {
+        strncpy(this->nombre,nombre,NAME_MAX_CHAR);
+        retorno = 0;
+    }
+    return retorno;
+}
+//-----------------------------------------------
+int Emp_getNombre(Empleado* this, char* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        strncpy(resultado,this->nombre,NAME_MAX_CHAR);
+        retorno = 0;
+    }
+    return retorno;
+}
+//---------------------------------------------
+int Emp_setEstado(Empleado* this, int estado)
+{
+    int retorno = -1;
+    if(this != NULL && estado >= 0)
+    {
+        this->estado = estado;
+        retorno = 0;
+    }
+    return retorno;
+}
+//----------------------------------------------
+
+int Emp_getEstado(Empleado* this, int* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->estado;
+        retorno = 0;
+    }
+    return retorno;
+}
+//----------------------------------------------
+int emp_comprarNombre(Empleado* this, Empleado* this1)
+{
+    int retorno = 1;
+    if(this != NULL && this1 != NULL)
+    {
+        retorno = strcmp(this->nombre,this1->nombre);
+    }
+    return retorno;
+}
+//-------------------------------------------------
+
+
+
+
+
---
+++ miguel_clases/memoria/memoria/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "empleado.h"
+
+int main()
+{
+    Empleado *pArrayEmpleado[1000];
+    int ultimoElementoArrayEmpleado = 0;
+    int indiceActual;
+    int auxInt;
+    char auxNombre[31];
+    float auxPeso;
+    int algo;
+
+    indiceActual = ultimoElementoArrayEmpleado;
+    pArrayEmpleado[indiceActual] =  Emp_new();
+
+
+    if(pArrayEmpleado[indiceActual] != NULL)
+    {
+
+        ultimoElementoArrayEmpleado++;
+        printf("OK\n");
+        if(!Emp_setId(pArrayEmpleado[indiceActual],14))
+        {
+            Emp_getId(pArrayEmpleado[indiceActual], &auxInt);
+            printf("El id es %d\n",auxInt);
+
+        }
+        if(!Emp_setPeso(pArrayEmpleado[indiceActual],50.50))
+        {
+            Emp_getPeso(pArrayEmpleado[indiceActual],&auxPeso);
+            printf("El peso es %.2f\n",auxPeso);
+        }
+        if(!Emp_setNombre(pArrayEmpleado[indiceActual],"juan"))
+        {
+            Emp_getNombre(pArrayEmpleado[indiceActual],auxNombre);
+            printf("Nombre: %s\n",auxNombre);
+        }
+
+    }
+    printf("indice 2 %d",indiceActual);
+    if(pArrayEmpleado[indiceActual]!= NULL)
+    {
+        ultimoElementoArrayEmpleado++;
+        printf("OK");
+        if(Emp_setNombre(pArrayEmpleado[indiceActual],"juanG"))
+        {
+            Emp_getNombre(pArrayEmpleado[indiceActual],auxNombre);
+            printf("%d",emp_comprarNombre(pArrayEmpleado[indiceActual-1],pArrayEmpleado[indiceActual]));
+
+
+        }
+    }
+
+    return 0;
+}
